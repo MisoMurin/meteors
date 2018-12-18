957769cd256bc006b723a743e46b45b67316c657
Michal Murin <mmurin.dev@gmail.com>
2018-12-18 20:51:58 +0100

    Init and add project

---
+++ app/src/main/java/com/murin/meteors/MeteorsActivity.kt
@@ -0,0 +1,11 @@
+package com.murin.meteors
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MeteorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meteors)
+    }
+}
